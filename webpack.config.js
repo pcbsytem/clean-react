7d0afa800fad5dcc564b3fd45722a814c20c6838
Patrick <pkcs@gft.com>
2024-07-08 07:11:04 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -7,22 +7,48 @@ module.exports = {
   output: {
     path: path.join(__dirname, "public/js"),
     publicPath: "/public/js",
-    filename: "bundle.js",
+    filename: "bundle.js"
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: [".ts", ".tsx", ".js", "scss"],
     alias: {
-      "@": path.join(__dirname, "src"),
-    },
+      "@": path.join(__dirname, "src")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
   },
   devServer: {
     contentBase: "./public",
     writeToDisk: true,
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   externals: {
     react: "React",
-    "react-dom": "ReactDOM",
+    "react-dom": "ReactDOM"
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [new CleanWebpackPlugin()]
 };
