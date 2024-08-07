cdbe03d823268d1ad98dc2c1e03196eeba8de64e
Patrick <pkcs@gft.com>
2024-08-07 08:46:59 -0300

    feat: add Login composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Router } from '@/presentation/components'
 import '@/presentation/styles/global.scss'
+import { makeLogin } from './factories/pages/login/login-factory'
 
 const root = createRoot(document.getElementById('main'))
 root.render(
   <React.StrictMode>
-    <Router />
+    <Router MakeLogin={makeLogin} />
   </React.StrictMode>
 )
--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Login } from '@/presentation/pages'
 
-const Router: FC = () => {
+type Props = {
+  MakeLogin: FC
+}
+
+const Router: FC<Props> = ({ MakeLogin }: Props) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' Component={Login} />
+        <Route path='/login' element={<MakeLogin />} />
       </Routes>
     </BrowserRouter>
   )
