c88edb99faaea3b1e90ca27f0f243a659c299d61
Patrick <pkcs@gft.com>
2024-08-30 05:15:48 -0300

    fix: set right name file for api url factory

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -8,7 +8,7 @@ import {
   RenderResult,
   waitFor
 } from '@testing-library/react'
-import Login from './login'
+import { Login } from '@/presentation/pages'
 import { AuthenticationSpy, ValidationStub } from '@/presentation/test'
 import { InvalidCredentialsError } from '@/domain/errors'
 
