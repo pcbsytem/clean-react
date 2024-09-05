af74539b01b83b4c351e5e256be39f03b857ad34
Patrick <pkcs@gft.com>
2024-09-05 07:05:24 -0300

    chore: rename faker call

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,12 +1,12 @@
 import { AuthenticationParams } from '@/domain/usecases'
 import { AccountModel } from '../models'
-import fake from 'faker'
+import faker from 'faker'
 
 export const mockAuthentication = (): AuthenticationParams => ({
-  email: fake.internet.email(),
-  password: fake.internet.password()
+  email: faker.internet.email(),
+  password: faker.internet.password()
 })
 
 export const mockAccountModel = (): AccountModel => ({
-  accessToken: fake.random.uuid()
+  accessToken: faker.random.uuid()
 })
