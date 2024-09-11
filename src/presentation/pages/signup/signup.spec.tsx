0231ea375dc133df19a0954420ef36be5f7366bc
Patrick <pkcs@gft.com>
2024-09-11 06:57:15 -0300

    test: ensure SignUp present error if SaveAccessToken fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -195,4 +195,15 @@ describe('SignUp Component', () => {
     )
     expect(mockUsedNavigate).toHaveBeenCalledWith('/', { replace: true })
   })
+
+  test('Should present error if SaveAccessToken fails', async () => {
+    const { sut, saveAccessTokenMock } = makeSut()
+    const error = new EmailInUseError()
+    jest.spyOn(saveAccessTokenMock, 'save').mockRejectedValueOnce(error)
+    await simulateValidSubmit(sut)
+    await waitFor(() => {
+      Helper.testElementText(sut, 'main-error', error.message)
+    })
+    Helper.testChildCount(sut, 'error-wrap', 1)
+  })
 })
