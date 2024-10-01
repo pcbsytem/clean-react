efd56ce780c8166a53c062761021aa77dc6278d3
Patrick <pkcs@gft.com>
2024-10-01 07:50:36 -0300

    test: ensure SignUp prevent submit if form is invalid

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -86,4 +86,10 @@ describe('Cypress TS', () => {
     FormHelper.testUrl('/')
     FormHelper.testLocalStorageItem('accessToken')
   })
+
+  it('Should not call submit if form is invalid', () => {
+    Http.mockOk()
+    cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}')
+    cy.get('@request.all').should('have.length', 0)
+  })
 })
