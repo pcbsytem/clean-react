753d8d821219e807003b16327378d364d1f68d80
Patrick <pkcs@gft.com>
2024-11-11 07:22:42 -0300

    test; ensure CurrentAccountAdapter calls LocalStorageAdapter with correct values

---
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -0,0 +1,14 @@
+import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
+import { setCurrentAccountAdapter } from './current-account-adapter'
+import { mockAccountModel } from '@/domain/test'
+
+jest.mock('@/infra/cache/local-storage-adapter')
+
+describe('CurrentAccountAdapter', () => {
+  test('Should call LocalStorageAdapter with correct value', () => {
+    const account = mockAccountModel()
+    const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
+    setCurrentAccountAdapter(account)
+    expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+})
